4314dfe2bae20ec62170e5be4f602d25d1fe630f
amandajcustodio <amandajcustodio@outlook.com>
2022-07-31 11:56:09 -0300

    commit(notes): class 31-07

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { enviroment } from './environment/environment';
+import { AuthModule } from './modules/auth/auth.module';
+import { NoteCommentModule } from './modules/note-comment/note-comment.module';
+import { NoteLikeModule } from './modules/note-like/note-like.module';
+import { NoteRoutingModule } from './modules/note/note-routing.module';
+import { NoteModule } from './modules/note/note.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
@@ -13,6 +18,10 @@ import { UserModule } from './modules/user/user.module';
       logging: enviroment.DATABASE_LOGGING === 'true',
     }),
     UserModule,
+    AuthModule,
+    NoteCommentModule,
+    NoteLikeModule,
+    NoteRoutingModule,
   ],
 })
 export class AppModule {}
